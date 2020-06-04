1b3a25e195487a0e29941a5712d5135b84dcd965
mob-sakai <sakai861104@gmail.com>
2020-06-04 12:01:11 +0900

    refactor: refactor

--- Packages/SoftMaskForUGUI/Scripts/Editor/SoftMaskableEditor.cs
+++ Packages/SoftMaskForUGUI/Scripts/Editor/SoftMaskableEditor.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEditor;
-using System.Linq;
 using MaskIntr = UnityEngine.SpriteMaskInteraction;
 
 namespace Coffee.UISoftMask
