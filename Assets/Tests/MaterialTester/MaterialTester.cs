c8f276805be4ffa701db03dcce538a3032fcdf01
mob-sakai <sakai861104@gmail.com>
2020-09-27 17:30:07 +0900

    test: add test for graphic.materialForRendering

---
+++ Assets/Tests/MaterialTester/MaterialTester.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MaterialTester
+{
+    [MenuItem("CONTEXT/Graphic/Test Material For Rendering")]
+    static void TestMaterialForRendering(MenuCommand command)
+    {
+        var graphic = command.context as Graphic;
+        if (!graphic) return;
+
+        var first = graphic.materialForRendering;
+        Debug.LogFormat("First: {0}, {1}", first, first.GetInstanceID());
+
+        var second = graphic.materialForRendering;
+        Debug.LogFormat("First: {0}, {1}", second, second.GetInstanceID());
+
+        Debug.Assert(first == second);
+    }
+}
